83ff70fa834a0b1d7a4ab5dad33818c49a866552
Ubuntu <oleges@olegesDS.jtdlto0ah4oepdi1lkw3k5xq0d.bx.internal.cloudapp.net>
2019-11-24 11:13:15 +0000

    face2simpsons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from train import *
 import yaml
 
-with open('configs/UTKFace2music_unpaired.yaml', 'r') as f:
+with open('configs/face2simpson_unpaired.yaml', 'r') as f:
     config = DotDict(yaml.safe_load(f))
     
 train(config)
